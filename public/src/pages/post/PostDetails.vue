3f996ff829ffd4ceab3ea966efc661a62e4a38ff
Revi1337 <david122123@gmail.com>
2023-09-01 23:03:25 +0900

    TEST
    TEST

--- public/src/pages/post/PostDetails.vue
+++ public/src/pages/post/PostDetails.vue
@@ -89,11 +89,15 @@ onMounted(() => {
   console.log('onMounted()');
 });
 
+const preventCallStack = ref('');
 onUpdated(() => {
-  summary.value = Array.from(markdownHtml.value.children).filter(child =>
-    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(child.tagName.toLowerCase())
-  );
-  window.addEventListener('scroll', scrollHandler);
+  if (preventCallStack.value === '') {
+    summary.value = Array.from(markdownHtml.value.children).filter(child =>
+      ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(child.tagName.toLowerCase())
+    );
+    window.addEventListener('scroll', scrollHandler);
+    preventCallStack.value === true;
+  }
 });
 
 onBeforeUnmount(() => {
