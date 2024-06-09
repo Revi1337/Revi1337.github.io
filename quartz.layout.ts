5ce33fee0385e5e4f89ab3fae8639b6b4fb5f58e
Revi1337 <david122123@gmail.com>
2024-06-09 19:42:33 +0900

    Quartz sync: Jun 9, 2024, 7:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -63,9 +63,36 @@ export const defaultContentPageLayout: PageLayout = {
           })
         }
         if (a.file && !b.file) {
-          return -1
-        } else {
           return 1
+        } else {
+          // return -1
+          const nameOrderMap: Record<string, number> = {
+            "Language": 100,
+            "Algorithm": 101,
+            "ComputerScience": 102,
+            "Framework": 103,
+            "DesignPattern": 104,
+            "Settings": 105,
+            "SSAFY": 106,
+            "interview": 107
+          }
+
+          let orderA = 0
+          let orderB = 0
+
+          if (a.file && a.file.slug) {
+            orderA = nameOrderMap[a.file.slug] || 0
+          } else if (a.name) {
+            orderA = nameOrderMap[a.name] || 0
+          }
+
+          if (b.file && b.file.slug) {
+            orderB = nameOrderMap[b.file.slug] || 0
+          } else if (b.name) {
+            orderB = nameOrderMap[b.name] || 0
+          }
+
+          return orderA - orderB
         }
       }
       // sortFn: (a, b) => {
