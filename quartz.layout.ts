eaf16e084705c25e932b290fe4b94088be4e0fe6
Revi1337 <david122123@gmail.com>
2024-06-09 19:40:21 +0900

    Quartz sync: Jun 9, 2024, 7:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -53,6 +53,21 @@ export const defaultContentPageLayout: PageLayout = {
           }
         }
       },
+      sortFn: (a, b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+        if (a.file && !b.file) {
+          return -1
+        } else {
+          return 1
+        }
+      }
       // sortFn: (a, b) => {
       //   const nameOrderMap: Record<string, number> = {
       //     "Language": 100,
