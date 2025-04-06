0a0a8761df37a24a1ae9512cf4715cf2f59f5059
Revi1337 <david122123@gmail.com>
2025-04-06 17:05:51 +0900

    Quartz sync: Apr 6, 2025, 5:05 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -202,6 +202,7 @@ export const defaultContentPageLayout: PageLayout = {
           "AWS/4_S3BucketForBackend": 9504,
           "AWS/5_S3BucketCloudFrontForBackend": 9505,
           "AWS/6_EC2": 9506,
+          "AWS/7_EC2_RDS_Connect": 9507,
           "AWS/8_EC2ConnectDomain": 9508,
           "AWS/9_EC2SupplyHttps": 9509,
           "AWS/11_RDS": 9511,
